cb6c0f534eb58130b0c9b939003e809a9f75086b
Dele <oladele@gmail.com>
2013-06-08 16:33:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatternsApp::Application.config.secret_token = '1ff4149f3aa5c7570daf7623c124a24f316cd53611ea9ba3c0ecb08d713172c68f5e5804334ec556d2022d5ad7351bb88251af69296769d5954696d326cf42cc'
