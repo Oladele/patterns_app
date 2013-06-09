295bbcdc154d2e47316808a594b45e0a8a906488
Dele <oladele@gmail.com>
2013-06-09 08:32:07 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,29 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	subject { page }
   describe "Home page" do
-
-    it "should have the title 'PatternsApp | Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', text: 'PatternsApp | Home')
-    end
-
-    it "should have the h1 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', text: 'Home')
-    end
+  	before { visit root_path }
+    it { should have_selector('title', text: 'PatternsApp | Home') }
+    it { should have_selector('h1', text: 'Home') }
   end
 
   describe "Help page" do
-
-  	it "should have the title 'PatternsApp | Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', text: 'PatternsApp | Help')
-    end
-
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', text: 'Help')
-    end	
+  	before { visit help_path }
+  	it { should have_selector('title', text: 'PatternsApp | Help') }
+    it { should have_selector('h1', text: 'Help') }
   end
 
   describe "About page" do
-
-  	it "should have the title 'PatternsApp | About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', text: 'PatternsApp | About')
-    end
-
-    it "should have the h1 'About Us" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', text: 'About Us')
-    end
+  	before { visit about_path }
+  	it { should have_selector('title', text: 'PatternsApp | About') }
+    it { should have_selector('h1', text: 'About Us') }
   end
+
+  describe "Contact page" do
+  	before { visit contact_path }
+  	it { should have_selector('title', text: 'PatternsApp | Contact') }
+    it { should have_selector('h1', text: 'Contact') }
+  end
+  
 end
